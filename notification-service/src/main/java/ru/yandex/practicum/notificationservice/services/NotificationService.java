9bfecc79c8257abc446b83037480fcccb476dc16
Lubov1 <lubov.88867@gmail.com>
2025-08-23 17:22:40 +0200

    notification service added

---
+++ notification-service/src/main/java/ru/yandex/practicum/notificationservice/services/NotificationService.java
@@ -0,0 +1,53 @@
+package ru.yandex.practicum.notificationservice.services;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+@Service
+public class NotificationService {
+    @Value("${mail.smtp.host}")
+    private String smtpHost;
+    @Value("${mail.smtp.port}")
+    private String smtpPort;
+    @Value("${mail.smtp.email}")
+    private String smtpEmail;
+    @Value("${mail.smtp.password}")
+    private String smtpPassword;
+
+    public void sendEmail(String email, String message) {
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.port", smtpPort);
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(smtpEmail, smtpPassword);
+            }
+        });
+
+        try {
+            // Создание сообщения
+            Message emailMessage = new MimeMessage(session);
+            emailMessage.setFrom(new InternetAddress(smtpEmail));
+            emailMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            emailMessage.setSubject("Уведомление");
+            emailMessage.setText(message);
+
+            // Отправка
+            Transport.send(emailMessage);
+
+            System.out.println("Письмо отправлено успешно!");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
